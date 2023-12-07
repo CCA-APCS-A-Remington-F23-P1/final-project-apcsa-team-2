b5d0ce0842165d09127c435f0ea1a06acee6032d
yanj4777 <152903762+yanj4777@users.noreply.github.com>
2023-12-07 09:00:51 -0800

    update Game.java - graphics stuff + update() + paint()

--- Game.java
+++ Game.java
@@ -17,6 +17,7 @@ public class Game extends Canvas implements KeyListener, Runnable
   // private Character char;
   private List<Balloon> balloons;
   private List<Obstacles> obstacles;
+  private List<Obstacles> bullets;
 
   // pause/end game
   private boolean pause = false;
@@ -30,13 +31,64 @@ public class Game extends Canvas implements KeyListener, Runnable
 
   //timing
   private long lastObstacle; //seconds since the last obstacle was summoned
+  private long lastBullet; //seconds since the last bullet was summoned
   private long lastBalloon; //seconds since the last balloon was summoned
 
-  // //constructor
-  // public Game() {
-  //   setBackground(Color.black); // maybe we can change the background in the future?
-  //   keys = new boolean[1];
-  // }
+  //constructor
+  public Game(String charChoice) {
+    setBackground(Color.black); // maybe we can change the background in the future?
+    keys = new boolean[1];
+
+    // char = new Character(charChoice, x, y, w, h);
+    balloons = new List<Balloons>;
+    obstacles = new List<Obstacles>;
+    bullets = new List<Obstacles>;
+
+    this.addKeyListener(this);
+    new Thread(this).start();
+
+    setVisible(true);  
+  }
+
+  public void update(Graphics window)
+  {
+    paint(window);
+  }
+
+  public void paint(Graphics window) {
+    if (end) {
+      return;
+    }
+    if (pause) {
+      if(keys[0]) {
+        pause = false;
+        keys[0] = false;
+      }
+      else {
+        return;
+      }
+    }
+
+    Graphics2D twoDGraph = (Graphics2D)window;
+    if (back==null) {
+      back = (BufferedImage)(createImage(getWidth(),getHeight()));
+    }
+    Graphics graphToBack = back.createGraphics();
+    graphToBack.setColor(Color.PURPLE);
+    graphToBack.drawString("game title", 25, 50);
+    graphToBack.setColor(Color.BLACK);
+    graphToBack.fillRect(0,0,800,600);
+
+    // char.draw(graphToBack);
+    for (Balloon b: balloons) {
+      b.draw();
+    }
+    for (Obstacle o : obstacles) {
+      o.draw();
+    }
+    for (Obstacle o : bullets) {
+      o.draw();
+    }
 
     
 
