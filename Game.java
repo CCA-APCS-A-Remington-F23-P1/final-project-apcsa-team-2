dac01197647561d95d4a80431141ae03d219f26b
yanj4777 <152903762+yanj4777@users.noreply.github.com>
2023-12-07 08:39:53 -0800

    update Game.java - add more private variables + start constructor

--- Game.java
+++ Game.java
@@ -13,16 +13,32 @@ import java.util.ArrayList;
 public class Game extends Canvas implements KeyListener, Runnable
 {
   //uncomment when ready
+  
   // private Character char;
   private List<Balloon> balloons;
   private List<Obstacles> obstacles;
 
   // pause/end game
-  boolean pause = false;
-  boolean end = false;
-
-  private boolean[] keys;
-  private BufferedImage back;
+  private boolean pause = false;
+  private boolean end = false;
   private int score = 0;
   private int lives = 0;
+
+  //functionality
+  private boolean[] keys;
+  private BufferedImage back;
+
+  //timing
+  private long lastObstacle; //seconds since the last obstacle was summoned
+  private long lastBalloon; //seconds since the last balloon was summoned
+
+  // //constructor
+  // public Game() {
+  //   setBackground(Color.black); // maybe we can change the background in the future?
+  //   keys = new boolean[1];
+  // }
+
+    
+
+  
 }
