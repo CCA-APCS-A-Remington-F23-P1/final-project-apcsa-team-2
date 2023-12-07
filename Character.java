b67d969bff7759f5eb3e470bac93f95bf96fb353
yanj4777 <152903762+yanj4777@users.noreply.github.com>
2023-12-07 09:21:27 -0800

    createCharacter.java

---
+++ Character.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.util.List;
+
+public class Character 
+{
+  private Image image;
+
+  public Character() {
+    
+  }
+}
