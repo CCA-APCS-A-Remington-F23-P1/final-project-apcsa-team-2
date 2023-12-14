614d2c7e50924341aa5e87354a79320eff94461c
lehercca <152903827+lehercca@users.noreply.github.com>
2023-12-14 16:50:08 +0000

    fixed up some errors on game.java

--- Game.java
+++ Game.java
@@ -18,11 +18,15 @@ public class Game extends Canvas implements KeyListener, Runnable
 {
   //uncomment when ready
   
-  // private Character char;
+  private Character charter;
   private BalloonSet balloons;
-  private List<Obstacle> obstacles;
-  private List<Obstacle> bullets;
+  private ObstacleSet obstacles;
+  private ObstacleSet bullets;
+  private BufferedImage backg;
 
+  private String bgLocation = "/images/Parallax_No_Car.gif";
+  private File bg;
+  
   // pause/end game
   private boolean pause = false;
   private boolean end = false;
@@ -31,30 +35,45 @@ public class Game extends Canvas implements KeyListener, Runnable
 
   //functionality
   private boolean[] keys;
-  private File bg = new File("/images/Parallax_No_Car.gif");
-  private BufferedImage back = ImageIO.read(bg);
-
+  
   //timing
   private long lastObstacle; //seconds since the last obstacle was summoned
   private long lastBullet; //seconds since the last bullet was summoned
-  // private long lastBalloon; //seconds since the last balloon was summoned
+  private long lastBalloon; //seconds since the last balloon was summoned
 
   //constructor
   public Game(String charChoice) {
+    try {
+    bg = new File(bgLocation);
+    } catch (Exception f) {
+      System.out.println("couldn't open bg 1");
+    }
+
+    try {
+      backg = ImageIO.read(bg);
+    } catch (IOException e) {
+      System.out.println("couldn't load background 2");
+    }
+  
+
     setBackground(Color.black); // maybe we can change the background in the future?
     keys = new boolean[1];
 
     // char = new Character(charChoice, x, y, w, h);
     balloons = new BalloonSet();
-    obstacles = new ArrayList<Obstacle>();
-    bullets = new ArrayList<Obstacle>();
+    obstacles = new ObstacleSet();
+    bullets = new ObstacleSet();
+
+    lastBalloon = 0;
+    lastObstacle = 0;
+    lastBullet = 0;
 
     this.addKeyListener(this);
     new Thread(this).start();
 
     setVisible(true); 
   
-    Character charter = new Character();
+    Character charter = new Character(charChoice, 20, 20, 20, 20,3);
 
   }
 
@@ -78,10 +97,10 @@ public class Game extends Canvas implements KeyListener, Runnable
     }
 
     Graphics2D twoDGraph = (Graphics2D)window;
-    if (back==null) {
-      back = (BufferedImage)(createImage(getWidth(),getHeight()));
+    if (backg==null) {
+      backg = (BufferedImage)(createImage(getWidth(),getHeight()));
     }
-    Graphics graphToBack = back.createGraphics();
+    Graphics graphToBack = backg.createGraphics();
     graphToBack.setColor(Color.BLUE);
     graphToBack.drawString("game title", 25, 50);
     graphToBack.setColor(Color.BLACK);
@@ -89,12 +108,8 @@ public class Game extends Canvas implements KeyListener, Runnable
 
     // char.draw(graphToBack);
     balloons.update();
-    for (Obstacle o : obstacles) {
-      o.draw();
-    }
-    for (Obstacle o : bullets) {
-      o.draw();
-    }
+    obstacles.draw(graphToBack);
+    bullets.draw(graphToBack);
     graphToBack.drawString("score : " + score, 25, 25);
     graphToBack.drawString("lives : " + lives, 25, 50);
 
@@ -109,13 +124,16 @@ public class Game extends Canvas implements KeyListener, Runnable
       lastObstacle = System.currentTimeMillis();
     }
 
+    if(System.currentTimeMillis() - lastBullet > Math.random()*300+100) {
+      obstacles.add(new Obstacle("bullet", 100, 100, 10, 10, 5));
+      lastObstacle = System.currentTimeMillis();
+    }
+
     //things that need to be added:
-    //add balloon after some amount of seconds (use lastBalloon and System.currentTimeMillis())
-    //adding bullets after some amount of seconds (use lastBullet and System.currentTimeMillis())
     // getting hit and losing a life
     // getting hit and losing the game
 
-    twoDGraph.drawImage(back,null,0,0);
+    twoDGraph.drawImage(backg,null,0,0);
   }
 
   public void keyPressed(KeyEvent e) {
